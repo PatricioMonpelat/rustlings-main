1b751f1cafc20b3c22f484d6285064089b51b8f1
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-04-11 15:47:08 -0300

    update

--- exercises/functions/functions3.rs
+++ exercises/functions/functions3.rs
@@ -4,11 +4,12 @@
 // I AM NOT DONE
 
 fn main() {
-    call_me();
+    call_me(3);
 }
 
 fn call_me(num: u32) {
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
+    
 }
