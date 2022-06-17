159b104a5b31e005d2f9fdd7d76b3b0c73bfd6c3
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-06-17 16:18:26 -0300

    struct 3 excercise

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -17,6 +17,7 @@ impl Package {
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
             // panic statement goes here...
+            panic!()
         } else {
             Package {
                 sender_country,
@@ -26,12 +27,14 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self) -> bool {
         // Something goes here...
+        &self.sender_country != &self.recipient_country
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    fn get_fees(&self, cents_per_kg: i32) -> i32 {
         // Something goes here...
+        (&self.weight_in_grams * cents_per_kg  + (1000 / 2) ) / 1000
     }
 }
 
@@ -73,7 +76,7 @@ mod tests {
         let sender_country = String::from("Spain");
         let recipient_country = String::from("Spain");
 
-        let cents_per_gram = 3;
+        let cents_per_gram = 3000;
 
         let package = Package::new(sender_country, recipient_country, 1500);
 
